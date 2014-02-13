cbd999eb7f5dfb0f4e0ab4525a5c28054edda860
Rich Harris <richard.a.harris@gmail.com>
2014-02-12 23:52:17 -0500

    use grunt-sass instead of grunt-contrib-sass, so we can stay in node the whole time

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,7 @@ module.exports = function ( grunt ) {
 	});
 
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-spelunk' );
