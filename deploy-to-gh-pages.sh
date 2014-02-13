e83e802f605d818ef715f3a22d9cc2e0eb58dcf3
Rich Harris <richard.a.harris@gmail.com>
2014-02-12 23:25:57 -0500

    more debugging

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -3,10 +3,11 @@
 # deploy script based on https://medium.com/philosophy-logic/53a8270e87db
 echo "Deploy script running..."
 ( cd build
-	git init
-	git config user.name "Travis-CI"
-	git config user.email "richard.a.harris+travis@gmail.com"
-	git add .
-	git commit -m "Deployed to Github Pages"
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+	ls
+#	git init
+#	git config user.name "Travis-CI"
+#	git config user.email "richard.a.harris+travis@gmail.com"
+#	git add .
+#	git commit -m "Deployed to Github Pages"
+#	git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
