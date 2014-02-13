7537ce19bafdf0f36a3f1146e7f643ce9ed84710
Rich Harris <richard.a.harris@gmail.com>
2014-02-12 23:32:01 -0500

    added deploy sequence back in

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -5,11 +5,11 @@ echo "Deploy script running..."
 ( cd build
 	# grunt copy task doesn't seem to work here...
 	cp ../root/* ./
-	ls
-#	git init
-#	git config user.name "Travis-CI"
-#	git config user.email "richard.a.harris+travis@gmail.com"
-#	git add .
-#	git commit -m "Deployed to Github Pages"
-#	git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+	git init
+	git config user.name "Travis-CI"
+	git config user.email "richard.a.harris+travis@gmail.com"
+	git add .
+	git commit -m "Deployed to Github Pages"
+	git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
