f26bf000a85131d9f711c7e419edfa5f211938f9
Rich Harris <richard.a.harris@gmail.com>
2014-04-04 00:45:38 -0400

    Squashed 'shared/' content from commit adbec82
    git-subtree-dir: shared
    git-subtree-split: adbec82b17f63f420eae6513701d39c09d55b361

---
+++ js/utils/nav.js
@@ -0,0 +1,19 @@
+define([
+
+], function (
+
+) {
+
+	'use strict';
+
+	var expanded, ul;
+
+	ul = document.querySelector( 'nav ul' );
+
+	// TODO fastclick on mobile
+	document.querySelector( '#nav-toggle' ).addEventListener( 'click', function () {
+		expanded = !expanded;
+		ul.style.height = expanded ? 'auto' : '1px';
+	});
+
+});
