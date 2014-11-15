cea90afa52ba0dd0992f0726b54c0214b94a23d3
Chris Reeves <chris@ev-soft.net>
2014-11-16 10:55:45 +1100

    add an edge version that doesn't overlap with latest so that the cdn naming convention and the docs naming conventions match

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function ( grunt ) {
 	config = {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		latest: '0.6',
-		edge: '0.6',
+		edge: 'edge',
 		prod: grunt.option( 'prod' ),
 
 		// TODO do we need this?... probably not, it just got
